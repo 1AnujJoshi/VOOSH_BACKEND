26fad65ec6ebb57445de7c3d46f8fe3cb75f4a47
anujdtechex2111 <anujjoshi.87210@gmail.com>
2023-01-29 18:02:24 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ const { addOrder, getOrder } = require("../controllers/order_controller");
 console.log("router loaded");
 const { checkToken } = require("../config/jwt.config");
 const { asyncHandler } = require("../helpers/error-handler");
-// router.get("/", userApi.index);
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to voosh-order-api" });
+});
 router.post("/add-user", addUser);
 router.post("/login-user", loginUser);
 router.post("/add-order", asyncHandler(checkToken), addOrder);
